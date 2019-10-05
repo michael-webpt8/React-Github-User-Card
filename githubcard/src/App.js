989d0a7db88092f9dd9c841237576866d134445b
Michael Williams <manicdesigner@gmail.com>
2019-10-05 14:21:24 -0400

    loading data with axios in App.js

--- githubcard/src/App.js
+++ githubcard/src/App.js
@@ -1,16 +1,51 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import GithubCard from './components/GithubCard';
+import Container from '@material-ui/core/Container';
+import './App.css';
 class App extends Component {
-	constructor() {
-		super();
-	}
+  constructor() {
+    super();
+    this.state = {
+      github: []
+    };
+  }
 
-	render() {
-		return (
-			<>
-				<h3>hi</h3>
-			</>
-		);
-	}
+  componentDidMount() {
+    this.gitHubUser();
+  }
+
+  gitHubUser = () => {
+    Axios.get(`https://api.github.com/users/mikeyjwilliams`)
+      .then(res =>
+        this.setState({
+          github: res.data
+        })
+      )
+      .catch(err => console.log('Error', err));
+  };
+
+  render() {
+    console.log('data', this.state.github);
+    return (
+      <>
+        <CssBaseline />
+        <Container maxWidth='lg'>
+          <h2 className='text-center'>Github API</h2>
+          <GithubCard github={this.state.github} />
+        </Container>
+      </>
+    );
+  }
 }
 export default App;
+
+// bio
+// followers:
+// following_url: => link
+// location:
+// login
+// name
+// public repos
+// avatar_url  => image
