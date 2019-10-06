8daeb05702bf275f8cf5c3a4cf3b4f788455f724
Michael Williams <manicdesigner@gmail.com>
2019-10-05 21:12:25 -0400

    added followers following, public repos, & gists

--- githubcard/src/components/GithubCard.js
+++ githubcard/src/components/GithubCard.js
@@ -71,81 +71,64 @@ function GithubCard(props) {
   if (!user) return <h2>Loading...</h2>;
 
   return (
-    <Card className={classes.card}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label='recipe' className={classes.avatar}>
-            {firstLetter(user.name)}
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label='settings'>
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={user.name}
-        subheader={user.login}
-      />
-      <CardMedia
-        className={classes.media}
-        image={user.avatar_url}
-        title={user.name}
-      />
-      <CardContent>
-        <Typography variant='body2' color='textSecondary' component='p'>
-          Location: {user.location}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label='Github'>
-          <Icon className='fab fa-github fa-2x' color='primary' />
-        </IconButton>
-        <IconButton aria-label='share'>
-          <ShareIcon />
-        </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label='show more'
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout='auto' unmountOnExit>
+    <>
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label='recipe' className={classes.avatar}>
+              {firstLetter(user.name)}
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label='settings'>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title={user.name}
+          subheader={user.login}
+        />
+        <CardMedia
+          className={classes.media}
+          image={user.avatar_url}
+          title={user.name}
+        />
         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-            set aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-            stirring occasionally until lightly browned, 6 to 8 minutes.
-            Transfer shrimp to a large plate and set aside, leaving chicken and
-            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-            onion, salt and pepper, and cook, stirring often until thickened and
-            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-            cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is
-            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
-            shrimp and mussels, tucking them down into the rice, and cook again
-            without stirring, until mussels have opened and rice is just tender,
-            5 to 7 minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then
-            serve.
+          <Typography variant='body2' color='textSecondary' component='p'>
+            Location: {user.location}
           </Typography>
         </CardContent>
-      </Collapse>
-    </Card>
+        <CardActions disableSpacing>
+          <a href={user.html_url}>
+            <IconButton aria-label='Github'>
+              <Icon className='fab fa-github fa-2x' color='primary' />
+            </IconButton>
+          </a>
+
+          <IconButton aria-label='share'>
+            <ShareIcon />
+          </IconButton>
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label='show more'
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout='auto' unmountOnExit>
+          <CardContent>
+            <Typography paragraph>{user.bio}</Typography>
+            <Typography paragraph>Followers: {user.followers}</Typography>
+            <Typography paragraph>Following: {user.following}</Typography>
+            <Typography paragraph>Public Repos: {user.public_repos}</Typography>
+            <Typography paragraph>Public Gist: {user.public_gists}</Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </>
   );
 }
 export default GithubCard;
